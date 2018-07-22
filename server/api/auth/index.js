b878db423273372458180a5921ff6293b70642be
rajkadiyala <rskadiy@gmail.com>
2018-07-22 00:08:29 -0700

    Abstracting user routes logic into service

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -1,35 +1,23 @@
 const router = require('express').Router();
-const User = require('../../db/models/User');
+const {userService} = require('../../services');
 
 module.exports = router;
 
 router.post('/login', async (req, res, next) => {
-    const user = await User.findOne({
-        where: {
-            email: req.body.email,
-        },
-    });
-    if (!user) {
-        console.log('No such user found:', req.body.email);
-        res.status(401).send('Wrong username and/or password');
-    } else if (!user.correctPassword(req.body.password)) {
-        console.log('Incorrect password for user:', req.body.email);
-        res.status(401).send('Wrong username and/or password');
-    } else {
-        req.login(user, err => (err ? next(err) : res.json(user)));
+    try {
+        const user = await userService.login(req.body);
+        req.login(user, e => (e ? next(e) : res.json(user)));
+    } catch (e) {
+        res.status(e.status).send(e.statusText);
     }
 });
 
 router.post('/signup', async (req, res, next) => {
     try {
-        const user = await User.create(req.body);
-        req.login(user, err => (err ? next(err) : res.json(user)));
-    } catch (err) {
-        if (err.name === 'SequelizeUniqueConstraintError') {
-            res.status(401).send('User already exists');
-        } else {
-            next(err);
-        }
+        const user = await userService.signup(req.body);
+        req.login(user, e => (e ? next(e) : res.json(user)));
+    } catch (e) {
+        res.status(e.status).send(e.statusText);
     }
 });
 
