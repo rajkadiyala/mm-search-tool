5f47acca1862f2c3e18ccbefff703e1afafd9392
rajkadiyala <rskadiy@gmail.com>
2018-07-22 00:14:39 -0700

    Revert "Abstracted user routes logic into service"
    This reverts commit 700233a9fad0ec6c77f49d318f282ad47e12f000.

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -1,23 +1,35 @@
 const router = require('express').Router();
-const {userService} = require('../../services');
+const User = require('../../db/models/User');
 
 module.exports = router;
 
 router.post('/login', async (req, res, next) => {
-    try {
-        const user = await userService.login(req.body);
-        req.login(user, e => (e ? next(e) : res.json(user)));
-    } catch (e) {
-        res.status(e.status).send(e.statusText);
+    const user = await User.findOne({
+        where: {
+            email: req.body.email,
+        },
+    });
+    if (!user) {
+        console.log('No such user found:', req.body.email);
+        res.status(401).send('Wrong username and/or password');
+    } else if (!user.correctPassword(req.body.password)) {
+        console.log('Incorrect password for user:', req.body.email);
+        res.status(401).send('Wrong username and/or password');
+    } else {
+        req.login(user, err => (err ? next(err) : res.json(user)));
     }
 });
 
 router.post('/signup', async (req, res, next) => {
     try {
-        const user = await userService.signup(req.body);
-        req.login(user, e => (e ? next(e) : res.json(user)));
-    } catch (e) {
-        res.status(e.status).send(e.statusText);
+        const user = await User.create(req.body);
+        req.login(user, err => (err ? next(err) : res.json(user)));
+    } catch (err) {
+        if (err.name === 'SequelizeUniqueConstraintError') {
+            res.status(401).send('User already exists');
+        } else {
+            next(err);
+        }
     }
 });
 
--- server/index.js
+++ server/index.js
@@ -67,10 +67,9 @@ function createApp() {
         res.sendFile(path.join(__dirname, '..', 'dist/index.html'));
     });
 
-    app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-        console.log('*****REACHED ERROR HANDLING ENDWARE*****');
-        console.error('ERROR MESSAGE: ', err);
-        console.error('ERROR STACK: ', err.stack);
+    app.use((err, req, res, next) => { /* eslint-disable-line no-unused-vars */
+        console.error(err);
+        console.error(err.stack);
         res.status(err.status || 500).send(err.message || 'Internal server error.');
     });
 }
